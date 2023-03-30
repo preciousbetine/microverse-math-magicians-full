0cbd5286f34924b7b00f9ab0417878796aaf4e77
Precious Betine <preciousbetine@gmail.com>
2023-03-30 03:53:50 +0100

    Add quote component

--- src/routes/Calculator.jsx
+++ src/routes/Calculator.jsx
@@ -3,7 +3,8 @@ import CalculatorComponent from '@/components/Calculator';
 
 const Calculator = () => {
   return (
-    <div>
+    <div className="calculator-container">
+      <h2>Let's do some math</h2>
       <CalculatorComponent />
     </div>
   )
